f9f3cfceff64676f43425a2928ec6d2bec0ed689
Michael Pollmeier <michael@michaelpollmeier.com>
2018-08-24 09:05:02 +0200

    leave gpg setup to the user

--- plugin/src/main/scala/com/geirsson/CiReleasePlugin.scala
+++ plugin/src/main/scala/com/geirsson/CiReleasePlugin.scala
@@ -21,52 +21,37 @@ object CiReleasePlugin extends AutoPlugin {
 
   def isTravisTag: Boolean =
     Option(System.getenv("TRAVIS_TAG")).exists(_.nonEmpty)
-  def isTravisSecure: Boolean =
-    System.getenv("TRAVIS_SECURE_ENV_VARS") == "true"
-
-  def setupGpg(): Unit = {
-    (s"echo ${sys.env("PGP_SECRET")}" #| "base64 --decode" #| "gpg --import").!
-  }
 
   override def buildSettings: Seq[Def.Setting[_]] = List(
-    dynverSonatypeSnapshots := true,
-    pgpPassphrase := sys.env.get("PGP_PASSPHRASE").map(_.toCharArray())
+    dynverSonatypeSnapshots := true
   )
 
   override def globalSettings: Seq[Def.Setting[_]] = List(
     publishArtifact.in(Test) := false,
     publishMavenStyle := true,
     commands += Command.command("ci-release") { currentState =>
-      if (!isTravisSecure) {
-        println("No access to secret variables, doing nothing")
-        currentState
-      } else {
-        println(
-          s"Running ci-release.\n" +
-            s"  TRAVIS_SECURE_ENV_VARS=${sys.env("TRAVIS_SECURE_ENV_VARS")}\n" +
-            s"  TRAVIS_BRANCH=${sys.env("TRAVIS_BRANCH")}\n" +
-            s"  TRAVIS_TAG=${sys.env("TRAVIS_TAG")}"
-        )
-        setupGpg()
-        if (!isTravisTag) {
-          if (isSnapshotVersion(currentState)) {
-            println(s"No tag push, publishing SNAPSHOT")
-            sys.env.getOrElse("CI_SNAPSHOT_RELEASE", "+publish") ::
-              currentState
-          } else {
-            // Happens when a tag is pushed right after merge causing the master branch
-            // job to pick up a non-SNAPSHOT version even if TRAVIS_TAG=false.
-            println(
-              "Snapshot releases must have -SNAPSHOT version number, doing nothing"
-            )
+      println(
+        s"Running ci-release.\n" +
+          s"  TRAVIS_SECURE_ENV_VARS=${sys.env("TRAVIS_SECURE_ENV_VARS")}\n" +
+          s"  TRAVIS_BRANCH=${sys.env("TRAVIS_BRANCH")}\n" +
+          s"  TRAVIS_TAG=${sys.env("TRAVIS_TAG")}"
+      )
+      if (!isTravisTag) {
+        if (isSnapshotVersion(currentState)) {
+          println(s"No tag push, publishing SNAPSHOT")
+          sys.env.getOrElse("CI_SNAPSHOT_RELEASE", "+publish") ::
             currentState
-          }
         } else {
-          println("Tag push detected, publishing a stable release")
-          sys.env.getOrElse("CI_RELEASE", "+publishSigned") ::
-            s"sonatypeRelease" ::
-            currentState
+          // Happens when a tag is pushed right after merge causing the master branch
+          // job to pick up a non-SNAPSHOT version even if TRAVIS_TAG=false.
+          println("Snapshot releases must have -SNAPSHOT version number, doing nothing")
+          currentState
         }
+      } else {
+        println("Tag push detected, publishing a stable release")
+        sys.env.getOrElse("CI_RELEASE", "+publishSigned") ::
+          s"sonatypeRelease" ::
+          currentState
       }
     }
   )
