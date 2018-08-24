388e55f1fbee797a94a0b9a499e3780c41b365e2
Michael Pollmeier <michael@michaelpollmeier.com>
2018-08-24 09:15:14 +0200

    leave decision to publish/not-publish to .travis.yml

--- plugin/src/main/scala/com/geirsson/CiReleasePlugin.scala
+++ plugin/src/main/scala/com/geirsson/CiReleasePlugin.scala
@@ -19,9 +19,6 @@ object CiReleasePlugin extends AutoPlugin {
   override def requires =
     JvmPlugin && SbtPgp && DynVerPlugin && GitPlugin && Sonatype
 
-  def isTravisTag: Boolean =
-    Option(System.getenv("TRAVIS_TAG")).exists(_.nonEmpty)
-
   override def buildSettings: Seq[Def.Setting[_]] = List(
     dynverSonatypeSnapshots := true
   )
@@ -30,41 +27,13 @@ object CiReleasePlugin extends AutoPlugin {
     publishArtifact.in(Test) := false,
     publishMavenStyle := true,
     commands += Command.command("ci-release") { currentState =>
-      println(
-        s"Running ci-release.\n" +
-          s"  TRAVIS_SECURE_ENV_VARS=${sys.env("TRAVIS_SECURE_ENV_VARS")}\n" +
-          s"  TRAVIS_BRANCH=${sys.env("TRAVIS_BRANCH")}\n" +
-          s"  TRAVIS_TAG=${sys.env("TRAVIS_TAG")}"
-      )
-      if (!isTravisTag) {
-        if (isSnapshotVersion(currentState)) {
-          println(s"No tag push, publishing SNAPSHOT")
-          sys.env.getOrElse("CI_SNAPSHOT_RELEASE", "+publish") ::
-            currentState
-        } else {
-          // Happens when a tag is pushed right after merge causing the master branch
-          // job to pick up a non-SNAPSHOT version even if TRAVIS_TAG=false.
-          println("Snapshot releases must have -SNAPSHOT version number, doing nothing")
-          currentState
-        }
-      } else {
-        println("Tag push detected, publishing a stable release")
-        sys.env.getOrElse("CI_RELEASE", "+publishSigned") ::
-          s"sonatypeRelease" ::
-          currentState
-      }
+      println("Running ci-release")
+      "+publishSigned" :: "sonatypeRelease" :: currentState
     }
   )
 
   override def projectSettings: Seq[Def.Setting[_]] = List(
     publishTo := sonatypePublishTo.value
   )
-
-  def isSnapshotVersion(state: State): Boolean = {
-    version.in(ThisBuild).get(Project.extract(state).structure.data) match {
-      case Some(v) => v.endsWith("-SNAPSHOT")
-      case None    => throw new NoSuchFieldError("version")
-    }
-  }
 
 }
