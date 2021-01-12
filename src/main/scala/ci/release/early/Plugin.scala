c50b916c70ea2340476154d4c434b040dc268c4c
Alejandro Hernández <info@alejandrohdezma.com>
2021-01-12 14:42:21 +0100

    Convert `ciReleaseTagNextVersion` into a parametrised command

--- src/main/scala/ci/release/early/Plugin.scala
+++ src/main/scala/ci/release/early/Plugin.scala
@@ -27,14 +27,27 @@ object Plugin extends AutoPlugin {
     /* I tried to define these commands as tasks, but had the following errors:
      * - didn't know how to update the version within the task
      * - didn't figure out how to automatically cross-build without lot's of extra code
-    */
-    commands += Command.command("ciReleaseTagNextVersion") { state =>
-      val tag = Utils.determineAndTagTargetVersion(state.log.info(_), Increment.Patch).tag
-      Utils.push(tag, state.log.info(_))
-      state.log.info("reloading sbt so that sbt-git will set the `version`" +
-        s" setting based on the git tag ($tag)")
-      "verifyNoSnapshotDependencies" :: "reload" :: state
-      },
+     */
+    commands += Command.args("ciReleaseTagNextVersion", "--increase-major | --increase-minor | --increase-patch") { (state, args) =>
+      val maybeIncrement = args match {
+        case Seq("--increase-patch") | Nil => Some(Increment.Patch)
+        case Seq("--increase-minor")       => Some(Increment.Minor)
+        case Seq("--increase-major")       => Some(Increment.Major)
+        case _                             => None
+      }
+
+      maybeIncrement.map { increment =>
+        val tag = Utils.determineAndTagTargetVersion(state.log.info(_), increment).tag
+        Utils.push(tag, state.log.info(_))
+        state.log.info("reloading sbt so that sbt-git will set the `version`" +
+          s" setting based on the git tag ($tag)")
+        "verifyNoSnapshotDependencies" :: "reload" :: state
+      }.getOrElse {
+        state.log.error(s"Invalid argument/s `${args.mkString(" ")}`")
+        state.log.error("The only arguments allowed are `--increase-major`, `--increase-minor` and `--increase-patch`")
+        state.fail
+      }
+    },
     commands += Command.command("ciRelease") { state =>
       state.log.info("Running ciRelease")
       "verifyNoSnapshotDependencies" :: "+publish" :: state
